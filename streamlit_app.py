abccc165c71642c125d3010ba1f1713e397f0a71
KapaBravo <luisferreiracfo11@gmail.com>
2025-10-09 20:53:17 +0100

    StreamLit

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Set the page title
+st.title("Hello World!")
+
+# Display a simple message
+st.write("Welcome to your first Streamlit app!")
+
+# Add a friendly greeting
+st.write("This is a simple web page created with Streamlit.")
